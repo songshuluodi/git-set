2e67ccbe834c32a556eed873851b64ac99e7f8f4
Nzy <939601064@qq.com>
2022-03-01 01:41:19 +0800

    master commit2

--- src/main/java/com/bjpowernode/GitTest.java
+++ src/main/java/com/bjpowernode/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hellow git2");
         System.out.println("hellow git3");
         System.out.println("hellow git4");
+        System.out.println("master-test");
     }
 }

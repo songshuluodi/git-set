499b9c0bef0b29d81e10f5a0dcc3e008fb5d0528
Nzy <939601064@qq.com>
2022-03-01 00:48:55 +0800

    second commit

--- src/main/java/com/bjpowernode/GitTest.java
+++ src/main/java/com/bjpowernode/GitTest.java
@@ -3,5 +3,6 @@ package com.bjpowernode;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hellow git");
+        System.out.println("hellow git2");
     }
 }

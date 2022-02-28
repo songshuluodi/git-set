791e5c5a94b51259ef60a76f0fc3fbc0fe429eef
Nzy <939601064@qq.com>
2022-03-01 01:35:54 +0800

    hot-fxi commit1

--- src/main/java/com/bjpowernode/GitTest.java
+++ src/main/java/com/bjpowernode/GitTest.java
@@ -4,5 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hellow git");
         System.out.println("hellow git2");
+        System.out.println("hellow git3");
+        System.out.println("hellow git4");
     }
 }
